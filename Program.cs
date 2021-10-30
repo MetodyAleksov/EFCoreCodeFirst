ff2ce99dd048da4bb15e9b28f35dba45e8442515
Metody Aleksov <teddy57603@gmail.com>
2021-10-30 10:12:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
